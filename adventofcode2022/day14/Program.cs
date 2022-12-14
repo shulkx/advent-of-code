4b14e9cb3156899c46af2d1885cf409cdc17d6d4
Marcus Shu <46469249+shulkx@users.noreply.github.com>
2022-12-14 22:35:37 +0800

    aoc2022.day14: Fix format

--- adventofcode2022/day14/Program.cs
+++ adventofcode2022/day14/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Drawing;
+
 string input = File.ReadAllText("input.txt");
 
 Cave cave = new Cave(input, false);
@@ -10,64 +11,64 @@ Console.WriteLine($"Day 14 Part 2: {cave.LabelSands(new Point(500, 0))}");
 
 public class Cave
 {
-	private int _maxY;
-	private bool _hasFloor;
+    private int _maxY;
+    private bool _hasFloor;
 
     public Dictionary<Point, char> Map { get; set; } = new Dictionary<Point, char>();
 
-	public Cave(string input, bool hasFloor)
-	{
-		_hasFloor = hasFloor;
+    public Cave(string input, bool hasFloor)
+    {
+        _hasFloor = hasFloor;
 
         var scans = input.Split(Environment.NewLine);
 
-		foreach (var scan in scans)
-		{
+        foreach (var scan in scans)
+        {
             List<Point> rockPoints = new List<Point>();
 
             var coordinates = scan.Split(" -> ");
 
-			foreach (var coordinate in coordinates)
-			{
-				var axisValues = coordinate.Split(',');
-				
-				rockPoints.Add(new Point(
-					int.Parse(axisValues[0]), 
-					int.Parse(axisValues[1])
-					));
+            foreach (var coordinate in coordinates)
+            {
+                var axisValues = coordinate.Split(',');
+
+                rockPoints.Add(new Point(
+                    int.Parse(axisValues[0]),
+                    int.Parse(axisValues[1])
+                    ));
             }
 
-			for (int i = 1; i < rockPoints.Count; i++)
-			{
-				LabelRocks(rockPoints[i - 1], rockPoints[i]);
+            for (int i = 1; i < rockPoints.Count; i++)
+            {
+                LabelRocks(rockPoints[i - 1], rockPoints[i]);
             }
-		}
+        }
 
-		_maxY = Map.Max(x => x.Key.Y);
-	}
+        _maxY = Map.Max(x => x.Key.Y);
+    }
 
     public void LabelRocks(Point start, Point end)
     {
-		int xDelta = Math.Sign(end.X - start.X);
-		int yDelta = Math.Sign(end.Y - start.Y);
+        int xDelta = Math.Sign(end.X - start.X);
+        int yDelta = Math.Sign(end.Y - start.Y);
 
-		while (start != end)
-		{
+        while (start != end)
+        {
             Map[start] = '#';
-			start.Offset(xDelta, yDelta);
-		}
+            start.Offset(xDelta, yDelta);
+        }
 
-		if (!Map.ContainsKey(end))
+        if (!Map.ContainsKey(end))
         {
             Map.Add(end, '#');
         }
     }
 
-	public int LabelSands(Point sandStart)
-	{
-		while (true)
-		{
-			var sandLoc = SimulateSandUnit(sandStart);
+    public int LabelSands(Point sandStart)
+    {
+        while (true)
+        {
+            var sandLoc = SimulateSandUnit(sandStart);
 
             if (Map.ContainsKey(sandLoc))
             {
@@ -76,44 +77,44 @@ public class Cave
 
             // If has floor, cancel the condition "sandLoc.Y == _maxY + 1"
             if (!_hasFloor && sandLoc.Y == _maxY + 1)
-			{
-				break;
-			}
+            {
+                break;
+            }
 
-			Map[sandLoc] = 'o';
+            Map[sandLoc] = 'o';
         }
 
         return Map.Values.Count(x => x == 'o');
     }
 
-	public Point SimulateSandUnit(Point sand)
-	{
-		Point down = new Point(0, 1); ;
-		Point leftDown = new Point(-1, 1);
-		Point rightDown = new Point(1, 1);
+    public Point SimulateSandUnit(Point sand)
+    {
+        Point down = new Point(0, 1); ;
+        Point leftDown = new Point(-1, 1);
+        Point rightDown = new Point(1, 1);
 
-		while (sand.Y <= _maxY)
-		{
+        while (sand.Y <= _maxY)
+        {
 
             if (!Map.ContainsKey(MovePoint(sand, down)))
-			{
-				sand = MovePoint(sand, down);
+            {
+                sand = MovePoint(sand, down);
             }
-			else if (!Map.ContainsKey(MovePoint(sand, leftDown)))
-			{
+            else if (!Map.ContainsKey(MovePoint(sand, leftDown)))
+            {
                 sand = MovePoint(sand, leftDown);
             }
-			else if (!Map.ContainsKey(MovePoint(sand, rightDown)))
-			{
+            else if (!Map.ContainsKey(MovePoint(sand, rightDown)))
+            {
                 sand = MovePoint(sand, rightDown);
             }
-			else
-			{
-				break;
-			}
-		}
+            else
+            {
+                break;
+            }
+        }
         return sand;
     }
 
-	private Point MovePoint(Point point, Point offset) => new Point(point.X + offset.X, point.Y + offset.Y);
+    private Point MovePoint(Point point, Point offset) => new Point(point.X + offset.X, point.Y + offset.Y);
 }
